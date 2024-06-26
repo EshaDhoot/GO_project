172f15ddf43c7eba84d514cc8dad7c1e25fbf887
Esha <maheshwariesha11@gmail.com>
2024-06-26 17:03:36 +0530

    minor commit

--- routes/v1routes.go
+++ routes/v1routes.go
@@ -4,7 +4,7 @@ import (
 	"go_project/controllers"
 	"go_project/repository"
 	"go_project/services"
-
+"go_project/middlewares"
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -28,7 +28,7 @@ func Router(incomingRoutes *gin.Engine, mongoDB *mongo.Database) {
 		v1.POST("/users/refresh-token", authController.RefreshToken)
 
 		v1.POST("/products", productController.CreateProduct)
-		v1.GET("/products", productController.FetchProducts)
+		v1.GET("/products", middlewares.AuthenticateMiddleware, productController.FetchProducts)
 		v1.GET("/product/:id", productController.FetchProductById)
 		v1.DELETE("/product/:id", productController.FetchProductByIdAndDelete)
 
